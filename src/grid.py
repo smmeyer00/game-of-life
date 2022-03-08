872a6e2afe97200524233ad2c7aef0d8159d24ae
Steven Meyer <smmeyer00@gmail.com>
2022-03-07 19:59:01 -0600

    mvp

--- src/grid.py
+++ src/grid.py
@@ -17,18 +17,52 @@ class Grid:
         self.grid[row][col] = 0 
 
 
+    # returns neighbor count of cell at row, col
+    def neighbor_count(self, row, col):
+        offsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+        n = 0
+        for o in offsets:
+            r = row + o[0]
+            c = col + o[1]
+            if 0 <= r < len(self.grid) and 0 <= c < len(self.grid[0]) and self.grid[r][c] == 1:
+                n+=1
+
+        return n 
+
+
     # step to next state of game of life
     def step(self):
-        pass 
+        new_grid = [[0 for i in range(self.cols)] for i in range(self.rows)] 
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[0])):
+                if self.grid[row][col] == 1: # alive
+                    n = self.neighbor_count(row, col)
+                    if n == 2 or n == 3:
+                        # cell survives
+                        new_grid[row][col] = 1
+                    else:
+                        # cell dies
+                        new_grid[row][col] = 0 
+                else: # dead
+                    n = self.neighbor_count(row, col)
+                    if n == 3:
+                        # new cell spawns
+                        new_grid[row][col] = 1
+                    else:
+                        # cell stays dead
+                        new_grid[row][col] = 0
+
+        self.grid = new_grid
 
     
     # draw current grid to screen at (x, y) with provided size
-    def draw(self, screen, x, y, width, border_color, alive_color, dead_color):
-        block_size = width//len(self.grid)
+    def draw(self, screen, x, y, width, height, border_color, alive_color, dead_color):
+        block_width = width//len(self.grid[0])
+        block_height = height//len(self.grid)
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
-                outer_rect = pygame.Rect(i*block_size+x, j*block_size+y, block_size, block_size)
-                inner_rect = pygame.Rect(i*block_size + 1 + x, j*block_size + 1 + y, block_size-2, block_size-2)
+                outer_rect = pygame.Rect(j*block_width+x, i*block_height+y, block_width, block_height)
+                inner_rect = pygame.Rect(j*block_width + 1 + x, i*block_height + 1 + y, block_width-2, block_height-2)
                 pygame.draw.rect(screen, border_color, outer_rect)
                 pygame.draw.rect(screen, alive_color if self.grid[i][j]==1 else dead_color, inner_rect)
 
--- src/main.py
+++ src/main.py
@@ -1,29 +1,53 @@
 import pygame
+import time
 from grid import Grid 
 
 pygame.init()
 
 # globals 
-size = width, height = 1000, 1000
+size = width, height = 1200, 700
 screen = pygame.display.set_mode(size)
 
 black = 0, 0, 0
 white = 233, 238, 247
 grey = 90, 93, 97
 
-grid = Grid(100, 100)
+grid = Grid(height//10, width//10)
+grid.fill_cell(10, 10)
+grid.fill_cell(10, 11)
+grid.fill_cell(10, 12)
 
 
 
 def run():
+    t1 = time.perf_counter()
+    started = False 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:    
+                    started = not started  
+
+        if pygame.mouse.get_pressed()[0] and not started:
+            # fill cells 
+            pos = pygame.mouse.get_pos()
+            col = pos[0] // 10
+            row = pos[1] // 10
+            grid.fill_cell(row, col)
+
 
         screen.fill(grey)
-        grid.draw(screen, 0, 0, 1000, grey, black, white)
+
+        # only step every .1 seconds
+        if (time.perf_counter() - t1 > 0.2) and started:
+            grid.step()
+            t1 = time.perf_counter()
+
+        grid.draw(screen, 0, 0, 1200, 700, grey, black, white)
         pygame.display.flip() 
+
 
 
 if __name__ == "__main__":
